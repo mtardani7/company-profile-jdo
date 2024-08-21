062d2e8e31118aae668e8e6625a9d67bd17c3cf9
RIZKYANGKATA <rizmadoris@gmail.com>
2024-08-21 11:09:32 +0700

    footer_jdo

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+<head>
+  
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('assets/css/footer.css') }}">
+  <title>Contoh Footer</title>
+  <style>
+  </style>
+</head>
+<body> 
+  <footer class="footer">
+    <div class="container text-start" style="margin-top: 20px;"> 
+      <div class="row align-items-center">
+        <div class="col d-flex flex-column align-items-start">
+          <div class="logo-footer mb-3">
+            <img src="{{ asset('assets/images/logo.jpg') }}" alt="Rapid Plast Logo" style="height: 120px;">
+          </div>
+          <h6>Menjadi Penyedia Solusi Total 
+              Terkemuka di ASIA untuk Kemasan dan Plastik yang Inovatif, Ramah Lingkungan, dan Berteknologi Mutakhir
+          </h6>
+        </div>
+  
+        <div class="col" style="margin-left: 150px;">
+          <h4>Quick Links</h4>
+          <h6>> Home</h6>
+          <h6>> About Us</h6>
+          <h6>> Product</h6>
+          <h6>> Address</h6>
+        </div>
+  
+        <div class="sosmed col">
+          <div class="container text-start mb-4" style="font-size: 30px;">
+            <div class="row">
+              <div class="col">
+                <i class="bi bi-facebook"></i>
+              </div>
+              <div class="col">
+                <i class="bi bi-instagram"></i>
+              </div>
+              <div class="col">
+                <i class="bi bi-twitter"></i>
+              </div>
+              <div class="col">
+                <i class="bi bi-youtube"></i>
+              </div>
+            </div>
+          </div>
+  
+          <div class="container text-start" style="margin-top: 20px;">
+            <div class="contact-info">
+              <p><i class="bi bi-envelope-fill"></i> jeweldyna.oralcare@gmail.com</p>
+              <p><i class="bi bi-telephone-fill"></i> 082298115885</p>
+              <p><i class="bi bi-geo-alt-fill" style="margin-bottom: 70px"></i> Jl. Berbek Industri V No.21, Bebek, Berbek, Kec. Waru, Kabupaten Sidoarjo, Jawa Timur 61256</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>

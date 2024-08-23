9308283831c9b6587cf4778ac5eeb14a5c13896c
AlwanAlawi <alwanalawi1@gmail.com>
2024-08-23 10:35:26 +0700

    Coba

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AboutUsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layouts.navbar');
 });
+
+Route::get('/about_us', [AboutUsController::class, 'about_us']);
